14217a9a6e3bda7e2d4f2b74afbd624e6f715161
Vadim Saydakov <sayyydakov163@gmail.com>
2026-03-28 09:54:47 +0400

    senderAccountId, receiverAccountId,

--- Golden Crow/Models/Transaction.cs
+++ Golden Crow/Models/Transaction.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public int SenderId { get; set; }
+        public int SenderAccountId { get; set; }
 
-        public int ReceiverId { get; set; }
+        public int ReceiverAccountId { get; set; }
 
         public DateTime MyProperty { get; set; }
 
